748a37ca94a50da16447a6acc962c399c130a16d
Pierre Copois <pierre.copois@smals.be>
2022-08-24 13:34:05 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,52 @@
-import { NgModule } from '@angular/core';
+// modules
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
+// components
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductaddComponent } from './productadd/productadd.component';
+
+// services
+import { ProductService } from './product.service';
+import { ErrorInterceptorProvider } from './http.interceptor';
+import { DisablecontrolDirective } from './disablecontrol.directive';
+
+const appRoutes: Routes = [
+  { path: '', component: MainpageComponent },
+  { path: 'mainpage', component: MainpageComponent },
+  { path: 'productlist', component: ProductlistComponent },
+  { path: 'productadd', component: ProductaddComponent },
+  { path: 'productadd/:_id', component: ProductaddComponent },
+  // otherwise redirect to home
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainpageComponent,
+    ProductlistComponent,
+    ProductaddComponent,
+    DisablecontrolDirective
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    AlertModule.forRoot(),
+    HttpClientModule
+  ],
+  providers: [ProductService, ErrorInterceptorProvider],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
